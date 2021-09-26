588424ecc2400382c471a16c699bd84492f767c5
= <=>
2021-09-25 18:23:14 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# DEVELOPER: USAP
+
+from Tkinter import *
+import ttk
+import os
+import time
+import tkFont
+import tkMessageBox
+
+# Crear ventana
+window = Tk()
+window.title("Control E/S")
+window.geometry("400*300+0+0")
+
+window.mainloop()
