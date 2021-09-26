a135a1ff4e1cc2af3c2041fce85788275e6e3e12
= <=>
2021-09-25 18:47:05 -0600

    create on/off buttons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# DEVELOPER: USAP
+# DEVELOPER: Obed Paz - USAP
 
 from Tkinter import *
 import ttk
@@ -10,6 +10,17 @@ import tkMessageBox
 # Crear ventana
 window = Tk()
 window.title("Control E/S")
-window.geometry("400*300+0+0")
+window.geometry("400x300+0+0")
+
+def on():
+  print('on')
+
+def off():
+  print('off')
+
+# Crear botones
+btn_on = Button(window, text="Turn on", command=on).place(x=100,y=100)
+btn_off = Button(window, text="Turn off", command=off).place(x=100,y=150)
+
 
 window.mainloop()
