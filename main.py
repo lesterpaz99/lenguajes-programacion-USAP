a4b81720f37ee5ceb5ba94b74cc846e2742b9bac
= <=>
2021-10-02 16:04:52 -0600

    update main.py

--- main.py
+++ main.py
@@ -7,31 +7,19 @@ import tkMessageBox
 import os
 import time
 
-# Crear ventana
-window = Tk()
-window.title("Control I/O")
-window.geometry("600x400+0+0")
+# Crea ventana principal
+main_window = Tk()
+main_window.title("Control I/O")
+main_window.geometry("600x400+0+0")
 
-# Configurar texto
+# Configura texto
 text1 = tkFont.Font(family="Helvetica", size=1)
 text2 = tkFont.Font(family="Arial", size=14)
 
-def update():
-  pointer_file = open("/home/lesterpaz99/Documents/gpio_state.txt", "r")
-  for line in pointer_file:
-    field = line.split("\n")
-    value = field[0]
+# Agrega titulo a la ventana principal
+title = Label(main_window, text="Control GPIO", font=text1).place(x=100, y=10)
 
-    if (value == "1"):
-      label_on = Label(window, text="ON", font=text1).place(x=100, y=200)
-      window.after(1000, update)
-
-    if (value == "0"):
-      label_off = Label(window, text="OFF", font=text1).place(x=100, y=200)
-      window.after(1000, update)
-
-update()
-
+# Crea funciones de apagado y encendido
 def on():
   print('on')
   os.system("sudo /./home/lesterpaz99/Documents/myFirstAppUSAP/turn_on.sh")
@@ -40,12 +28,25 @@ def off():
   print('off')
   os.system("sudo /./home/lesterpaz99/Documents/myFirstAppUSAP/turn_off.sh")
 
-# Title
-title = Label(window, text="Control GPIO", font=text1).place(x=100, y=10)
+# Crea y agrega botones a la ventana principal
+btn_on = Button(main_window, text="Turn on", command=on).place(x=100,y=100)
+btn_off = Button(main_window, text="Turn off", command=off).place(x=100,y=150)
 
-# Crear botones
-btn_on = Button(window, text="Turn on", command=on).place(x=100,y=100)
-btn_off = Button(window, text="Turn off", command=off).place(x=100,y=150)
+def update():
+  pointer_file = open("/home/lesterpaz99/Documents/gpio_state.txt", "r")
+  for line in pointer_file:
+    field = line.split("\n")
+    value = field[0]
+
+    if (value == "1"):
+      label_on = Label(main_window, text="ON", font=text1).place(x=100, y=200)
+      main_window.after(1000, update)
+
+    if (value == "0"):
+      label_off = Label(main_window, text="OFF", font=text1).place(x=100, y=200)
+      main_window.after(1000, update)
+
+update()
 
 
-window.mainloop()
+main_window.mainloop()
