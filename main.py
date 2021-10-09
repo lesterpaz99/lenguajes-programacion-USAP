15f7e4577b10b39e6b193b791f218fc1bdf489fc
= <=>
2021-10-08 22:35:17 -0600

    add style guide for python code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tkFont
 import tkMessageBox 
 import os
 import time
+import subprocess
 
 # Crea ventana principal
 main_window = Tk()
@@ -21,30 +22,30 @@ title = Label(main_window, text="Control GPIO", font=text1).place(x=100, y=10)
 
 # Crea funciones de apagado y encendido
 def on():
-  print('on')
-  os.system("sudo /./home/lesterpaz99/Documents/myFirstAppUSAP/turn_on.sh")
+    print('on')
+    os.system("sudo /./home/lesterpaz99/Documents/myFirstAppUSAP/turn_on.sh")
 
 def off():
-  print('off')
-  os.system("sudo /./home/lesterpaz99/Documents/myFirstAppUSAP/turn_off.sh")
+    print('off')
+    os.system("sudo /./home/lesterpaz99/Documents/myFirstAppUSAP/turn_off.sh")
 
 # Crea y agrega botones a la ventana principal
 btn_on = Button(main_window, text="Turn on", command=on).place(x=100,y=100)
 btn_off = Button(main_window, text="Turn off", command=off).place(x=100,y=150)
 
 def update():
-  pointer_file = open("/home/lesterpaz99/Documents/gpio_state.txt", "r")
-  for line in pointer_file:
-    field = line.split("\n")
-    value = field[0]
+    pointer_file = open("/home/lesterpaz99/Documents/gpio_state.txt", "r")
+    for line in pointer_file:
+      field = line.split("\n")
+      value = field[0]
 
-    if (value == "1"):
-      label_on = Label(main_window, text="ON", font=text1).place(x=100, y=200)
-      main_window.after(1000, update)
+      if (value == "1"):
+        label_on = Label(main_window, text="ON", font=text1).place(x=100, y=200)
+        main_window.after(1000, update)
 
-    if (value == "0"):
-      label_off = Label(main_window, text="OFF", font=text1).place(x=100, y=200)
-      main_window.after(1000, update)
+      if (value == "0"):
+        label_off = Label(main_window, text="OFF", font=text1).place(x=100, y=200)
+        main_window.after(1000, update)
 
 update()
 
