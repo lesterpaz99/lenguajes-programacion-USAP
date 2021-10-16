4eb43cd58cd9578c1b7b681de0be432146da9c74
= <=>
2021-10-16 16:06:42 -0600

    create new file with the same output

--- turn_off.sh
+++ turn_off.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo 0 > /home/lesterpaz99/Documents/gpio_state.txt
+echo 0 > /home/lesterpaz99/Documents/gpio_state2.txt
--- turn_on.sh
+++ turn_on.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo 1 > /home/lesterpaz99/Documents/gpio_state.txt
+echo 1 > /home/lesterpaz99/Documents/gpio_state2.txt
